52cac54b72267b51151b312291cc17dbdcab0afd
Ayebakuro Ombu <xplicitkuro@gmail.com>
2019-12-26 09:59:41 +0100

    Added GET endpoints

--- routes/profile.js
+++ routes/profile.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 router.put("/bank", UserController.updateBank);
 router.put("/contact", UserController.updateContact);
-router.get("/bank", UserController.updateBank);
-router.get("/contact", UserController.updateContact);
+router.get("/bank", UserController.getBank);
+router.get("/contact", UserController.getContact);
 
 module.exports = router;
